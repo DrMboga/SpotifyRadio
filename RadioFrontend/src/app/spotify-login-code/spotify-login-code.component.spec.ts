4b81ec37535c6262510d286b8d68b101dca11db4
Michael Shabanov <dr.mboga@yahoo.com>
2025-06-19 16:02:43 +0200

    feat: frontend tests all dependencies mocked

--- RadioFrontend/src/app/spotify-login-code/spotify-login-code.component.spec.ts
+++ RadioFrontend/src/app/spotify-login-code/spotify-login-code.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SpotifyLoginCodeComponent } from './spotify-login-code.component';
+import { signal } from '@angular/core';
+import { SpotifySettings } from '../model/spotify-settings';
+import { SpotifyStore } from '../store/spotify.store';
+import { RouterModule } from '@angular/router';
 
 describe('SpotifyLoginCodeComponent', () => {
   let component: SpotifyLoginCodeComponent;
   let fixture: ComponentFixture<SpotifyLoginCodeComponent>;
 
+  let spotifyStoreMock = {
+    settings: signal<SpotifySettings>({}),
+    getAndSaveAuthenticationToken: jest.fn(),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SpotifyLoginCodeComponent]
-    })
-    .compileComponents();
+      imports: [SpotifyLoginCodeComponent, RouterModule.forRoot([])],
+      providers: [{ provide: SpotifyStore, useValue: spotifyStoreMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SpotifyLoginCodeComponent);
     component = fixture.componentInstance;
