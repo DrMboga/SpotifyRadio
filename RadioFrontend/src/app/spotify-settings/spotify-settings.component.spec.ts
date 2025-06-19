4ffedcfb18901fbc70e1ac73f0113e973fd7fc43
Michael Shabanov <dr.mboga@yahoo.com>
2025-06-19 10:34:57 +0200

    feat: frontend tests mock spotifySettings

--- RadioFrontend/src/app/spotify-settings/spotify-settings.component.spec.ts
+++ RadioFrontend/src/app/spotify-settings/spotify-settings.component.spec.ts
@@ -1,16 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SpotifySettingsComponent } from './spotify-settings.component';
+import { SpotifyStore } from '../store/spotify.store';
+import { SpotifySettings } from '../model/spotify-settings';
+import { signal } from '@angular/core';
+import { SpotifyDevice } from '../model/spotify-device';
+import { PlayListItem } from '../model/spotify-playlist-item';
 
 describe('SpotifySettingsComponent', () => {
   let component: SpotifySettingsComponent;
   let fixture: ComponentFixture<SpotifySettingsComponent>;
 
+  let spotifyStoreMock = {
+    settings: signal<SpotifySettings>({}),
+    devices: signal<SpotifyDevice[]>([]),
+    playLists: signal<PlayListItem[]>([]),
+    isAuthorized: signal<boolean>(false),
+    spotifyLoginUrl: signal<string>(''),
+    readSpotifyDevices: jest.fn(),
+    readPlayLists: jest.fn(),
+    saveSpotifySettings: jest.fn(),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SpotifySettingsComponent]
-    })
-    .compileComponents();
+      imports: [SpotifySettingsComponent],
+      providers: [{ provide: SpotifyStore, useValue: spotifyStoreMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SpotifySettingsComponent);
     component = fixture.componentInstance;
