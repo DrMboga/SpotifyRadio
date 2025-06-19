7f9c3299a968b457e11d5572eb49823d11464468
Michael Shabanov <dr.mboga@yahoo.com>
2025-06-19 17:28:46 +0200

    feat: frontend nav button component test

--- RadioFrontend/src/app/components/nav-button/nav-button.component.spec.ts
+++ RadioFrontend/src/app/components/nav-button/nav-button.component.spec.ts
@@ -2,9 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavButtonComponent } from './nav-button.component';
 import { RouterModule } from '@angular/router';
+import { ComponentRef } from '@angular/core';
 
 describe('NavButtonComponent', () => {
+  const route = 'home';
+
   let component: NavButtonComponent;
+  let componentRef: ComponentRef<NavButtonComponent>;
   let fixture: ComponentFixture<NavButtonComponent>;
 
   beforeEach(async () => {
@@ -14,10 +18,21 @@ describe('NavButtonComponent', () => {
 
     fixture = TestBed.createComponent(NavButtonComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    componentRef.setInput('routerLink', route);
+    componentRef.setInput('icon', 'home');
+    componentRef.setInput('label', 'Hi there');
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show normal mat-button when route is not matched', () => {
+    const button = fixture.nativeElement.querySelector('a');
+    expect(button.classList).toContain('mat-mdc-button');
+    expect(button.classList).not.toContain('mat-mdc-outlined-button');
   });
 });
