3643adcac5e2801c038e1493a9dee6da708fe3cd
Michael Shabanov <dr.mboga@yahoo.com>
2025-06-19 10:18:57 +0200

    feat: frontend mock radioStore

--- RadioFrontend/src/app/radio-stations/radio-stations.component.spec.ts
+++ RadioFrontend/src/app/radio-stations/radio-stations.component.spec.ts
@@ -2,20 +2,32 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RadioStationsComponent } from './radio-stations.component';
 import { RadioStore } from '../store/radio.store';
+import { RadioButtonInfo } from '../model/radio-button-info';
+import { signal } from '@angular/core';
+import { RadioButtonRegion } from '../model/radio-button-region';
+import { RadioStationInfo } from '../model/radio-station-info';
+import { RadioChannel } from '../model/radio-channel';
 
 describe('RadioStationsComponent', () => {
   let component: RadioStationsComponent;
   let fixture: ComponentFixture<RadioStationsComponent>;
-  let radioStore;
+  let radioStore = {
+    radioButtonsList: signal<RadioButtonInfo[]>([]),
+    regionsList: signal<string[]>([]),
+    radioButtonRegions: signal<RadioButtonRegion[]>([]),
+    sabaStationsList: signal<string[]>([]),
+    regionStationsList: signal<RadioStationInfo[]>([]),
+    sabaRadioChannels: signal<RadioChannel[]>([]),
+    getSabaRadioChannels: jest.fn(),
+    getRadioStationsByRegion: jest.fn(),
+    setRadioButtonRegion: jest.fn(),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RadioStationsComponent],
-      providers: [{ provide: RadioStore, useValue: {} }],
+      providers: [{ provide: RadioStore, useValue: radioStore }],
     }).compileComponents();
-
-    radioStore = TestBed.inject(RadioStore);
-    // https://ngrx.io/guide/signals/signal-store/testing#mocking-the-signalstore
 
     fixture = TestBed.createComponent(RadioStationsComponent);
     component = fixture.componentInstance;
