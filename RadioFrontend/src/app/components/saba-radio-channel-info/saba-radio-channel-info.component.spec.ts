e8d31b5361a03b9ffc3a663adeb19f53a7643cac
Michael Shabanov <dr.mboga@yahoo.com>
2025-06-19 17:40:31 +0200

    feat: frontend radio channel info component test scenarios scaffold

--- RadioFrontend/src/app/components/saba-radio-channel-info/saba-radio-channel-info.component.spec.ts
+++ RadioFrontend/src/app/components/saba-radio-channel-info/saba-radio-channel-info.component.spec.ts
@@ -8,9 +8,8 @@ describe('SabaRadioChannelInfoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SabaRadioChannelInfoComponent]
-    })
-    .compileComponents();
+      imports: [SabaRadioChannelInfoComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SabaRadioChannelInfoComponent);
     component = fixture.componentInstance;
@@ -20,4 +19,12 @@ describe('SabaRadioChannelInfoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // should show anchor if streamURL is filled
+
+  // should show input and button when streamURL is empty
+
+  // should call saveUrl method on button click and stream URL input is not empty
+
+  // should not call saveUrl method on button click and stream URL input is empty
 });
