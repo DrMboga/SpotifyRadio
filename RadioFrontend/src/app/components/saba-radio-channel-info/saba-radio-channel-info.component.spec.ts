181f78feb53b766f9afe6a5f8ebee99e407afaa5
Mikhail Shabanov <dr.mboga@yahoo.com>
2025-06-20 09:42:45 +0200

    feat: Frontend jest testing - radio channel component

--- RadioFrontend/src/app/components/saba-radio-channel-info/saba-radio-channel-info.component.spec.ts
+++ RadioFrontend/src/app/components/saba-radio-channel-info/saba-radio-channel-info.component.spec.ts
@@ -1,9 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { SabaRadioChannelInfoComponent } from './saba-radio-channel-info.component';
+import { ComponentRef } from '@angular/core';
+import { RadioChannel } from '../../model/radio-channel';
 
 describe('SabaRadioChannelInfoComponent', () => {
   let component: SabaRadioChannelInfoComponent;
+  let componentRef: ComponentRef<SabaRadioChannelInfoComponent>;
   let fixture: ComponentFixture<SabaRadioChannelInfoComponent>;
 
   beforeEach(async () => {
@@ -12,6 +15,7 @@ describe('SabaRadioChannelInfoComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(SabaRadioChannelInfoComponent);
+    componentRef = fixture.componentRef;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -20,11 +24,118 @@ describe('SabaRadioChannelInfoComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // should show anchor if streamURL is filled
+  it('should show anchor if streamURL is filled', fakeAsync(() => {
+    // Arrange
+    const radioChannel: RadioChannel = {
+      name: 'fake Radio',
+      streamUrl: 'http://fake-radio.com',
+      region: 'Bayern',
+      sabaFrequency: 89,
+      button: 1,
+    };
 
-  // should show input and button when streamURL is empty
+    componentRef.setInput('radioChannel', radioChannel);
+    tick();
 
-  // should call saveUrl method on button click and stream URL input is not empty
+    // Act
+    fixture.detectChanges();
 
-  // should not call saveUrl method on button click and stream URL input is empty
+    // Assert
+    const anchor = fixture.nativeElement.querySelector('a');
+    expect(anchor).toBeTruthy();
+    expect(anchor.textContent).toBe('Stream');
+    expect(anchor.getAttribute('href')).toBe(radioChannel.streamUrl);
+
+    const input = fixture.nativeElement.querySelector('input');
+    expect(input).toBeFalsy();
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button).toBeFalsy();
+  }));
+
+  it('should show input and button when streamURL is empty', fakeAsync(() => {
+    // Arrange
+    const radioChannel: RadioChannel = {
+      name: 'fake Radio',
+      region: 'Bayern',
+      sabaFrequency: 89,
+      button: 1,
+    };
+
+    componentRef.setInput('radioChannel', radioChannel);
+    tick();
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    const anchor = fixture.nativeElement.querySelector('a');
+    expect(anchor).toBeFalsy();
+    const input = fixture.nativeElement.querySelector('input');
+    expect(input).toBeTruthy();
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button).toBeTruthy();
+  }));
+
+  it('should emit savedRadioChannel output signal on button click and stream URL input is not empty', fakeAsync(() => {
+    // Arrange
+    const streamUrl = 'http://fake-radio.com';
+
+    const outputEventSpy = jest.spyOn(component.savedRadioChannel, 'emit');
+
+    const radioChannel: RadioChannel = {
+      name: 'fake Radio',
+      region: 'Bayern',
+      sabaFrequency: 89,
+      button: 1,
+    };
+    componentRef.setInput('radioChannel', radioChannel);
+    tick();
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+    const input = fixture.nativeElement.querySelector('input');
+    input.value = streamUrl;
+    input.dispatchEvent(new Event('input'));
+    tick();
+    fixture.detectChanges();
+
+    // Act
+    button.click();
+    tick();
+    fixture.detectChanges();
+
+    // Assert
+    expect(outputEventSpy).toHaveBeenCalledWith({
+      name: 'fake Radio',
+      region: 'Bayern',
+      sabaFrequency: 89,
+      button: 1,
+      streamUrl,
+    });
+  }));
+
+  it('should not emit savedRadioChannel output signal on button click and stream URL input is empty', fakeAsync(() => {
+    // Arrange
+    const outputEventSpy = jest.spyOn(component.savedRadioChannel, 'emit');
+
+    const radioChannel: RadioChannel = {
+      name: 'fake Radio',
+      region: 'Bayern',
+      sabaFrequency: 89,
+      button: 1,
+    };
+    componentRef.setInput('radioChannel', radioChannel);
+    tick();
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+
+    // Act
+    button.click();
+    tick();
+    fixture.detectChanges();
+
+    // Assert
+    expect(outputEventSpy).not.toHaveBeenCalled();
+  }));
 });
