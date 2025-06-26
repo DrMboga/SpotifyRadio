079a638b51efaa9a9118dcdb5e768457809644c5
Mikhail Shabanov <dr.mboga@yahoo.com>
2025-06-26 07:48:45 +0200

    feat: Frontend jest testing - login component

--- RadioFrontend/src/app/spotify-login-code/spotify-login-code.component.spec.ts
+++ RadioFrontend/src/app/spotify-login-code/spotify-login-code.component.spec.ts
@@ -4,21 +4,30 @@ import { SpotifyLoginCodeComponent } from './spotify-login-code.component';
 import { signal } from '@angular/core';
 import { SpotifySettings } from '../model/spotify-settings';
 import { SpotifyStore } from '../store/spotify.store';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { MOCK_SPOTIFY_SETTINGS } from '../mock/spotify-mock';
+import { of } from 'rxjs';
 
 describe('SpotifyLoginCodeComponent', () => {
   let component: SpotifyLoginCodeComponent;
   let fixture: ComponentFixture<SpotifyLoginCodeComponent>;
 
   let spotifyStoreMock = {
-    settings: signal<SpotifySettings>({}),
+    settings: signal<SpotifySettings>(MOCK_SPOTIFY_SETTINGS),
     getAndSaveAuthenticationToken: jest.fn(),
+  };
+
+  let mockRoute = {
+    queryParams: of({ code: 'fakeAuthCode' }),
   };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SpotifyLoginCodeComponent, RouterModule.forRoot([])],
-      providers: [{ provide: SpotifyStore, useValue: spotifyStoreMock }],
+      imports: [SpotifyLoginCodeComponent],
+      providers: [
+        { provide: SpotifyStore, useValue: spotifyStoreMock },
+        { provide: ActivatedRoute, useValue: mockRoute },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SpotifyLoginCodeComponent);
@@ -28,5 +37,13 @@ describe('SpotifyLoginCodeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should request and save authentication token', async () => {
+    expect(spotifyStoreMock.getAndSaveAuthenticationToken).toHaveBeenCalledWith({
+      code: 'fakeAuthCode',
+      clientId: MOCK_SPOTIFY_SETTINGS.clientId,
+      clientSecret: MOCK_SPOTIFY_SETTINGS.clientSecret,
+    });
   });
 });
