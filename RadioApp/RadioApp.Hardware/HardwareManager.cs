82ec275a3f5a75c225c21bf55e719058985e9d6d
Michael Shabanov <dr.mboga@yahoo.com>
2025-07-20 19:02:02 +0200

    feat: Backend GPIO Hardware worker

---
+++ RadioApp/RadioApp.Hardware/HardwareManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using RadioApp.Common.Hardware;
+using RadioApp.Hardware.PiGpio;
+
+namespace RadioApp.Hardware;
+
+public class HardwareManager: IHardwareManager
+{
+    private readonly ILogger<HardwareManager> _logger;
+
+    public HardwareManager(ILogger<HardwareManager> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Init()
+    {
+        GpioManager.GpioInitialize();
+        _logger.LogInformation("--== GPIO Initialized ==--");
+    }
+
+    public void Teardown()
+    {
+        GpioManager.GpioTerminate();
+        _logger.LogInformation("--== GPIO Terminated ==--");
+    }
+}
